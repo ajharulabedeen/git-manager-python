11447ab60857c1758b7225f62a2fa0cd084ac9de
ajharulabedeen <ajharulabedeen@gmail.com>
2021-02-10 09:48:56 +0600

    basic structure done!

--- git-manager.py
+++ git-manager.py
@@ -2,10 +2,27 @@ def hello():
     print("hello")
     name = str(input())
     if name:
-        print ("Hello " + str(name))
+        print("Hello " + str(name))
     else:
         print("Hello World")
     return
 
+
+def executeCommnad(command):
+    if command == "git init":
+        print("Repo inited");
+    elif command.startswith("git commit"):
+        print("commit success");
+    elif command == "git show all commit":
+        print("show all commits!");
+    else:
+        print("unknown command");
+    return
+
 while 1:
-    x=input();
+    command = input();
+    if command == "exit":
+        print("program exited");
+        break
+    else:
+        executeCommnad(command);
