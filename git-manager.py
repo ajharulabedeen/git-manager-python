30288cd310ec883e1c8c8162ed1a7003152bc8ac
ajharulabedeen <ajharulabedeen@gmail.com>
2021-02-10 10:29:29 +0600

    add commit, show all commit!

--- git-manager.py
+++ git-manager.py
@@ -1,27 +1,38 @@
-# test purpose, no use
-def hello():
-    print("hello")
-    name = str(input())
-    if name:
-        print("Hello " + str(name))
-    else:
-        print("Hello World")
-    return
+class commit:
+    def __init__(self, id, message):
+        self.id = id
+        self.message = message
+
+
+def addCommit(command):
+    split = command.split();
+    message = '';
+    for counter in range(len(split)):
+        if counter > 1:
+            message += split[counter] + " ";
+    list.append(commit(commandCounter, '"' + message + '"'))
+    print("commit success");
 
 
 def executeCommnad(command):
     if command == "git init":
         print("Repo inited");
     elif command.startswith("git commit"):
-        print("commit success");
+        addCommit(command)
     elif command == "git show all commit":
+        for obj in list:
+            print(obj.id, obj.message, sep=' ')
         print("show all commits!");
     else:
         print("unknown command");
     return
 
+
+list = [];
+commandCounter = 0;
 while 1:
     command = input();
+    commandCounter += 1;
     if command == "exit":
         print("program exited");
         break
