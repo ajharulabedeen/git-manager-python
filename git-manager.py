26bc0200f61cef162db2edf41374208846df29a8
ajharulabedeen <ajharulabedeen@gmail.com>
2021-02-10 09:37:10 +0600

    python file add!

---
+++ git-manager.py
@@ -0,0 +1,11 @@
+def hello():
+    print("hello")
+    name = str(input())
+    if name:
+        print ("Hello " + str(name))
+    else:
+        print("Hello World")
+    return
+
+while 1:
+    x=input();
